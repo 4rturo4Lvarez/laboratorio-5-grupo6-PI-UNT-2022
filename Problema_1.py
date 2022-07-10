f3579eebee79327615783cc1f6fc4cd0ed8739e7
4rturo4lvarez <t1023600321@unitru.edu.pe>
2022-07-10 10:40:27 -0500

    Problema modificado

--- Problema_1.py
+++ Problema_1.py
@@ -10,18 +10,21 @@ Cant_num, Cant_nulos, Sum_negat, Num, Prod_posit = 0, 0, 0, 0, 1
 
 Cant_num = int(input("Ingrese la cantidad de números del conjunto A: "))
 
-for i in range (1, Cant_num + 1):
-    print("Ingrese el número ", i, ": "); Num = int(input("----> "))
+print("\n")
+
+for i in range (Cant_num):
+    print("Ingrese el número ", i + 1, ": "); Num = int(input("----> "))
     
-    while Num < 0:
+    if Num < 0:
         Sum_negat = Num + Sum_negat
-        break
-    while Num > 0:
+    
+    elif Num > 0:
         Prod_posit = Num * Prod_posit
-        break
-    while Num == 0:
+    
+    else:
         Cant_nulos = Cant_nulos + 1
-        break
+
+print("\n")
 
 print("1) La sumatoria de los valores negativos no nulos de A es: ", Sum_negat)
 print("2) El producto de los valores positivos no nulos es: ", Prod_posit)
